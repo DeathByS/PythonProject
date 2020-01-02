093a40846985c608eaff528414aae846e6fc47f0
DeathByS <ghwhrlf@naver.com>
2020-01-02 17:30:59 +0900

    1/2

---
+++ Test.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+
+import sys
+from sync_Client import SyncClient 
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QTimer
+from PyQt5 import QtGui
+from PyQt5 import uic   
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSlot
+import time
+ 
+ 
+class Form(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.ui = uic.loadUi("form.ui", self)   
+        self.ui.show()
+        
+        # plc 와 연결하여 plc Data를 받아오는 객체
+        self.plcConnect = SyncClient()
+       
+        # QTimer 정해진 작업을 정해진 시간마다 반복하게 하기 위해.
+        # 여기서는 일정시간에 한번씩 plc에서 Data를 읽어오기 위하여 사용한다.
+        self.timer = QTimer(self)
+
+        # 버튼과 
+        self.startButton.clicked.connect(self.slotStartButton)
+        self.stopButton.clicked.connect(self.slotStopButton)
+        self.changeButton.clicked.connect(self.slotChangeButton)
+
+        # Plc Connect 와 Disconnect 버튼을 반복적으로 누르게 하지 않기 위해 체크하는 변수
+        self.isStart = False
+
+        # ip 
+        self.ip = "0"
+        
+    def changeLabelText(self):
+        coils, regs = self.plcConnect.runSyncClient()
+        self.ui.testLabel.setText(str(regs))
+    
+           
+    @pyqtSlot()
+    def slotStartButton(self):
+
+        if self.isStart == False:
+         self.isStart = True
+         self.plcConnect.connectClient(self.ip)
+         self.timer.setInterval(2000)
+         self.timer.start()
+         self.timer.timeout.connect(self.changeLabelText)
+        
+    @pyqtSlot()
+    def slotStopButton(self):
+        self.isStart = False
+        self.timer.stop()
+        self.plcConnect.closeClient()
+        #  self.ui.testLabel.setText("2st")
+ 
+    @pyqtSlot()
+    def slotChangeButton(self):
+         self.ip = self.ui.ipInput.text()
+         self.ui.ipLabel.setText(self.ip)
+ 
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = Form()
+    sys.exit(app.exec())
--- sync_Client.py
+++ sync_Client.py
@@ -1,27 +1,6 @@
-#!/usr/bin/env python
-"""
-Pymodbus Synchronous Client Examples
---------------------------------------------------------------------------
-The following is an example of how to use the synchronous modbus client
-implementation from pymodbus.
-It should be noted that the client can also be used with
-the guard construct that is available in python 2.5 and up::
-    with ModbusClient('127.0.0.1') as client:
-        result = client.read_coils(1,10)
-        print result
-"""
-# --------------------------------------------------------------------------- #
-# import the various server implementations
-# --------------------------------------------------------------------------- #
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-# from pymodbus.client.sync import ModbusUdpClient as ModbusClient
-# from pymodbus.client.sync import ModbusSerialClient as ModbusClient
-
-# --------------------------------------------------------------------------- #
-# configure the client logging
-# --------------------------------------------------------------------------- #
 import logging
-import time
+
 
 FORMAT = ('%(asctime)-15s %(threadName)-15s '
           '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
@@ -30,29 +9,61 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 UNIT = 0x1
+class SyncClient:
+    def __init__(self):
+        self.client = None
+        self.connectIp = "kwtkorea.iptime.org"
 
-def run_sync_client():
+    def connectClient(self, connectIp="kwtkorea.iptime.org"):
+        self.connectIp = connectIp
 
-    client = ModbusClient('kwtkorea.iptime.org', port=502)
- 
-    client.connect()
+        if self.client is None:
+            self.client = ModbusClient(self.connectIp, port=502) 
+            self.client.connect()
+            print("connect success")
+        
+        print("after connect", self.client)
 
-    while 1:
-    
+        return self.client
+
+    def closeClient(self):
+        if self.client is not None:
+            self.client.close()
+            self.client = None
+
+    def runSyncClient(self):
+
         log.debug("Write to a Coil and read back")
-        rr = client.read_coils(0, 10, unit=UNIT) 
-        assert(rr.bits[0] == True)          # test the expected value
-        log.debug("Write to a holding register and read back")
-        # rq = client.write_register(1, 10, unit=UNIT)
-        rr = client.read_holding_registers(0, 11, unit=UNIT)
+        
+        # rq = client.write_coil(0, False, unit=UNIT)
+        readCoils = self.client.read_coils(0, 10, unit=UNIT) 
+        print("rr.coil", readCoils.bits)
+        # assert(rr.bits[0] == True)          # test the expected value
+        # log.debug("Write to a holding register and read back")
+        # rq = client.write_register(30, 10, unit=UNIT)
+        readHoldingRegs = self.client.read_holding_registers(0, 11, unit=UNIT)
+        print("rr.registers", readHoldingRegs.registers)
         # assert(not rq.isError())     # test that we are not an error
-        print("rr.registers[0]", rr.registers)
-        time.sleep(2)
-    # ----------------------------------------------------------------------- #
-    # close the client
-    # ----------------------------------------------------------------------- #
-    client.close()
+        # print("rr.registers", rr.registers)
+        # ----------------------------------------------------------------------- #
+        # close the client
+        # ----------------------------------------------------------------------- #
+        # self.client.close()
+        # time.sleep(2)
+        return readCoils.bits, readHoldingRegs.registers
+        # return 
+
+# 코드가 인터프리터에 의해 직접 실행 될 때 실행되는 부분
+# if __name__ == "__main__":
+   
 
 
-if __name__ == "__main__":
-    run_sync_client()
+# test = SyncClient()
+
+# test.connectClient()
+
+# coils, holdingRegitsters = test.runSyncClient()
+
+# test.closeClient()
+
+# print(coils, holdingRegitsters)
